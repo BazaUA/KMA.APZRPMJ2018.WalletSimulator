41b2c3d1e10da1f0853a9daf4e3589bc74d3c007
m263313 <m263313@users.noreply.github.com>
2018-11-06 22:30:02 +0200

    Removed delete functions

--- WalletSimulator/ViewModels/MainViewViewModel.cs
+++ WalletSimulator/ViewModels/MainViewViewModel.cs
@@ -15,43 +15,25 @@ namespace KMA.APZRPMJ2018.RequestSimulator.ViewModels
     class MainViewViewModel : INotifyPropertyChanged
     {
         #region Fields
+
         private Request _selectedRequest;
 
         #region Commands
+
         private ICommand _addRequestCommand;
         private ICommand _logout;
-        private ICommand _deleteRequestCommand;
+
         #endregion
+
         #endregion
 
         #region Properties
+
         #region Commands
 
-        public ICommand AddRequestCommand
-        {
-            get
-            {
-                return _addRequestCommand ?? (_addRequestCommand = new RelayCommand<object>(AddRequestExecute));
-            }
-        }
+        public ICommand AddRequestCommand => _addRequestCommand ?? (_addRequestCommand = new RelayCommand<object>(AddRequestExecute));
 
-        public ICommand LogOut
-        {
-            get
-            {
-                return _logout ?? (_logout = new RelayCommand<object>(LogOutExecute));
-            }
-        }
-
-        
-
-        public ICommand DeleteRequestCommand
-        {
-            get
-            {
-                return _deleteRequestCommand ?? (_deleteRequestCommand = new RelayCommand<KeyEventArgs>(DeleteRequestExecute));
-            }
-        }
+        public ICommand LogOut => _logout ?? (_logout = new RelayCommand<object>(LogOutExecute));
 
         #endregion
 
@@ -70,17 +52,21 @@ namespace KMA.APZRPMJ2018.RequestSimulator.ViewModels
         #endregion
 
         #region Constructor
+
         public MainViewViewModel()
         {
             FillRequests();
             PropertyChanged += OnPropertyChanged;
         }
+
         #endregion
+
         private void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
         {
             if (propertyChangedEventArgs.PropertyName == "SelectedRequest")
                 OnRequestChanged(_selectedRequest);
         }
+
         private void FillRequests()
         {
             Requests = new ObservableCollection<Request>();
@@ -88,19 +74,6 @@ namespace KMA.APZRPMJ2018.RequestSimulator.ViewModels
             {
                 Requests.Add(Request);
             }
-            
-        }
-
-        private void DeleteRequestExecute(KeyEventArgs args)
-        {
-            if (args.Key != Key.Delete) return;
-
-            if (SelectedRequest == null) return;
-
-            StationManager.CurrentUser.Requests.RemoveAll(uwr => uwr.Guid == SelectedRequest.Guid);
-            FillRequests();
-            OnPropertyChanged(nameof(SelectedRequest));
-            OnPropertyChanged(nameof(Requests));
         }
 
         private void AddRequestExecute(object o)
@@ -123,7 +96,7 @@ namespace KMA.APZRPMJ2018.RequestSimulator.ViewModels
                     var filename = dlg.FileName;
                     var text = File.ReadAllText(filename);
                     var wordsCont = new WordsCont(text);
-             
+
                     var request = new Request(
                         filename,
                         wordsCont.NumberOfCharacters,
@@ -133,9 +106,9 @@ namespace KMA.APZRPMJ2018.RequestSimulator.ViewModels
                     Requests.Add(request);
                     _selectedRequest = request;
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
-                    MessageBox.Show(string.Format(Resources.SignIn_FailedToGetUser, Environment.NewLine,e.Message));
+                    MessageBox.Show(string.Format(Resources.SignIn_FailedToGetUser, Environment.NewLine, e.Message));
                     return;
                 }
             }
@@ -145,7 +118,6 @@ namespace KMA.APZRPMJ2018.RequestSimulator.ViewModels
                     "Invalid document"));
                 return;
             }
-
         }
 
         private void LogOutExecute(object o)
@@ -154,25 +126,32 @@ namespace KMA.APZRPMJ2018.RequestSimulator.ViewModels
         }
 
         #region EventsAndHandlers
+
         #region Loader
+
         internal event RequestChangedHandler RequestChanged;
+
         internal delegate void RequestChangedHandler(Request Request);
 
         internal virtual void OnRequestChanged(Request Request)
         {
             RequestChanged?.Invoke(Request);
         }
+
         #endregion
+
         #region PropertyChanged
+
         public event PropertyChangedEventHandler PropertyChanged;
+
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }  
-        #endregion
+        }
+
         #endregion
 
-
+        #endregion
     }
 }
